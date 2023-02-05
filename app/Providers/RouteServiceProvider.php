aa341fde68e6e32562ef4d00937eba16f2317c97
Mahdi <mahdibarati696@gmail.com>
2023-02-05 05:05:14 +0330

    Config Route and default string legnth

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,7 +34,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+                ->group(base_path('routes/web/main.php'));
+
+            Route::middleware(['web'])
+                ->group(base_path('routes/web/admin.php'));
         });
     }
 
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,27 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+
             $table->id();
+
             $table->string('name');
+
+            $table->string('username')->nullable()->unique();
+            
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+
+            $table->string('number', 11)->unique();
+            $table->timestamp('number_verified_at')->nullable();
+
             $table->string('password');
+
+            $table->timestamp('birth_day');
+
             $table->rememberToken();
+
             $table->timestamps();
+            
         });
     }
 

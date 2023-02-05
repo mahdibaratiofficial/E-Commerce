f41078edac233db0785320d48c09c0162d1505f9
Mahdi <mahdibarati696@gmail.com>
2023-02-05 05:09:00 +0330

    some change to reset Pass

--- app/Http/Controllers/Auth/ResetPassword.php
+++ app/Http/Controllers/Auth/ResetPassword.php
@@ -14,24 +14,21 @@ use Illuminate\Support\Facades\Hash;
 
 class ResetPassword extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
     public function generateAResetPassword(Request $request)
     {
 
-        $data = $request->validate([
-            'email' => ['required', 'exists:users,email']
+        $data = $request->validate(
+            [
+                'email' => ['required', 'exists:users,email']
             ]
         );
 
         if (!$data['email'])
             return 'reset page with errors';
 
-        $reset=$this->createRessetPasswordRow($data['email']);
+        $reset = $this->createRessetPasswordRow($data['email']);
 
-        $submitedResetEmail=PasswordReset::where('email', $reset['email'])->first();
+        $submitedResetEmail = PasswordReset::where('email', $reset['email'])->first();
 
         if ($submitedResetEmail)
             $this->sendResetPasswordLink($submitedResetEmail['email'], $submitedResetEmail['token']);
@@ -40,24 +37,24 @@ class ResetPassword extends Controller
 
     private function createRessetPasswordRow($email)
     {
-        $reset=PasswordReset::create([
-            'email'=>$data['email'],
-            'token'=>Str::random(60),
-            'expire_at'=>Carbon::now()->addMinutes(60)
+        $reset = PasswordReset::create([
+            'email' => $data['email'],
+            'token' => Str::random(60),
+            'expire_at' => Carbon::now()->addMinutes(60)
         ]);
 
         return $reset;
     }
 
 
-    public function sendResetPasswordLink($email,$token)
+    public function sendResetPasswordLink($email, $token)
     {
         return 'send Email';
     }
 
     public function ResetPasswordPage($token)
     {
-        $PR_token=PasswordReset::where('token', $token)->first();
+        $PR_token = PasswordReset::where('token', $token)->first();
 
         if ($PR_token < 1)
             return 'reset password page with token';
@@ -69,13 +66,13 @@ class ResetPassword extends Controller
 
     public function resetPassword(Request $request)
     {
-        $password=$request->validate([
+        $password = $request->validate([
             'password' => ['required', 'min:6'],
             'password_confirmation' => ['required', 'confirmed'],
-            '_token'=>['required','exists:password_resets,token']
+            '_token' => ['required', 'exists:password_resets,token']
         ]);
 
-        $token=PasswordReset::where('token', $password['_token'])->first();
+        $token = PasswordReset::where('token', $password['_token'])->first();
 
         if ($token < 1)
             return 'redirect to reset page with error';
@@ -83,16 +80,13 @@ class ResetPassword extends Controller
         if ($token['expire_at'] < Carbon::now())
             return 'redirect and say expire error';
 
+        // check if _token == url token
+
 
         User::update([
-            'password'=>Hash::make($password['password'])
+            'password' => Hash::make($password['password'])
         ]);
 
-        return 'ok';
-
+        return true;
     }
 }
-
-
-
-?>

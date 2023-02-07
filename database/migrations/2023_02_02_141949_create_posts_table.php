96f79951205a5f821002dddadb09088260efcaa9
Mahdi <mahdibarati696@gmail.com>
2023-02-07 10:16:32 +0330

    add user_id field

--- database/migrations/2023_02_02_141949_create_posts_table.php
+++ database/migrations/2023_02_02_141949_create_posts_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+            
             $table->string('title');
 
             $table->text('description');

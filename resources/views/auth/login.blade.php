1fb74063798e01aa8eb1c2aea8b596ab61e12228
Mahdi <mahdibarati696@gmail.com>
2023-02-07 10:17:22 +0330

    develop front-end of login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,132 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf_token" content="{{ csrf_token() }}">
     <title>Document</title>
+    @include('layouts.assets_path.main_styles')
 </head>
-<body>
-    <form action="{{ url('login') }}" method="POST">
-        @csrf
-        <input type="text" name="name">
-        <input type="password" name="password">
-        <input type="submit" >
-    </form>
+
+<body style="text-align: right">
+    <main class="main pages shabnam">
+        <div class="page-content pt-50">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-8 col-lg-10 col-md-12 m-auto">
+                        <div class="row card-login">
+                            <div class="col-lg-6 d-none d-lg-block">
+                                <div class="card-login mt-50">
+                                    <div class="heading_s2 mb-50">
+                                        <h4 class="mb-5 shabnam">ورود با روش های دیگه</h4>
+                                    </div>
+
+                                    <div class="dashboard-menu">
+                                        <ul class="nav flex-column" role="tablist">
+                                            <li class="nav-item">
+                                                <a class="nav-link " id="dashboard-tab" data-bs-toggle="tab"
+                                                    href="page-account.html#dashboard" role="tab"
+                                                    aria-controls="dashboard" aria-selected="false"><i
+                                                        class="fi-rs-phone-sliders mr-10"></i><span class="shabnam">ورود
+                                                        با شماره</span></a>
+                                            </li>
+
+                                            <li class="nav-item">
+                                                <a class="nav-link " id="userPassword-tab" data-bs-toggle="tab"
+                                                    href="page-account.html#userPassword" role="tab"
+                                                    aria-controls="dashboard" aria-selected="false"><i
+                                                        class="fi-rs-phone-sliders mr-10"></i><span class="shabnam">ورود با نام کاربری</span></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+
+                                    <a href="/oauth/google" class="social-login google-login"
+                                        style="text-align: center">
+                                        <img src="assets/main/imgs/theme/icons/logo-google.svg" alt="" />
+                                        <span class="shabnam">ورود با حساب گوگل</span>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-8">
+                                <div class="login_wrap widget-taber-content background-white">
+                                    <div class="padding_eight_all bg-white ">
+                                        <div class="heading_s1">
+                                            <h1 class="mb-5 shabnam">ورود</h1>
+                                            <p class="mb-30 shabnam">حساب کاربری نداری? <a href="page-register.html">یکی
+                                                    بساز</a></p>
+                                        </div>
+                                        <div class="tab-content account dashboard-content pl-50">
+                                            <div class="tab-pane" id="dashboard" role="tabpanel"
+                                                aria-labelledby="dashboard-tab">
+                                                <div class="card">
+                                                    <div class="card-header">
+                                                        <h4 class="mb-0 shabnam">شماره همراه خود را وارد کنید</h4>
+                                                            <form method="post">
+                                                                <div class="form-group mt-20">
+                                                                    <input type="text" required="" name="email"
+                                                                        placeholder="نام کاربری یا ایمیل یا شماره تلفن" />
+                                                                </div>
+
+                                                                <div class="form-group">
+                                                                    <button type="submit" class="btn btn-heading btn-block shabnam" style="width: 100%" name="login">ارسال پیامک</button>    
+                                                                </div>
+                                                            </form>
+
+                                                    </div>
+                                                    <div class="card-body">
+
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="tab-pane active" id="userPassword" role="tabpanel"
+                                                aria-labelledby="userPassword-tab">
+                                                <form method="post">
+                                                    <div class="form-group">
+                                                        <input type="text" required="" name="email"
+                                                            placeholder="نام کاربری یا ایمیل یا شماره تلفن" />
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <input required="" type="password" name="password"
+                                                            placeholder="گذرواژه" />
+                                                    </div>
+                                                    <div class="login_footer form-group text-center">
+                                                        جایگاه ربات گوگل
+                                                    </div>
+                                                    <div class="login_footer form-group mb-50">
+                                                        <div class="chek-form">
+                                                            <div class="custome-checkbox">
+                                                                <input class="form-check-input" type="checkbox"
+                                                                    name="checkbox" id="exampleCheckbox1"
+                                                                    value="" />
+                                                                <label class="form-check-label"
+                                                                    for="exampleCheckbox1"><span>منو
+                                                                        یادت باشه</span></label>
+                                                            </div>
+                                                        </div>
+                                                        <a class="text-muted shabnam" href="page-login.html#">رمز عبور
+                                                            خود را
+                                                            فراموش کرده اید?</a>
+                                                    </div>
+                                                    <div class="form-group" dir="rtl">
+                                                        <button type="submit"
+                                                            class="btn btn-heading btn-block hover-up shabnam"
+                                                            name="login">ورود</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
+@include('layouts.assets_path.main_scripts')
+
 </html>

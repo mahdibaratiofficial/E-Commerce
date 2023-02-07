6841acad46b4a9106c03f4eec65c245340529284
Mahdi <mahdibarati696@gmail.com>
2023-02-07 10:16:58 +0330

    activecode table for otp

--- database/migrations/2023_02_06_084945_create_active_codes_table.php
+++ database/migrations/2023_02_06_084945_create_active_codes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('active_codes', function (Blueprint $table) {
+            
             $table->unsignedBigInteger('user_id')->nullable();
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
 

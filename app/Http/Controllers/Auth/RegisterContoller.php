f00647980d53b9130a7093183ffbbfc139c1b19f
Mahdi <mahdibarati696@gmail.com>
2023-02-07 10:13:01 +0330

    Register Trait and Controller Develop

--- app/Http/Controllers/Auth/RegisterContoller.php
+++ app/Http/Controllers/Auth/RegisterContoller.php
@@ -5,13 +5,33 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Auth\Traits\Register;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use PhpParser\Node\Expr\FuncCall;
 
 class RegisterContoller extends Controller
 {
     use Register;
 
+
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
     public function register(Request $request)
     {
+        $data=$this->validation($request);
 
+        if ($data)
+        {
+            $user=User::create($data);
+
+            if (isset($user->id))
+                return redirect($this->redirectTo);
+            else
+                return redirect('register')->with(['create'], ['false']);
+        }
     }
 }
--- app/Http/Controllers/Auth/Traits/Register.php
+++ app/Http/Controllers/Auth/Traits/Register.php
@@ -6,7 +6,17 @@ use Illuminate\Http\Request;
 
 trait Register
 {
-    
+    public function validation(Request $request)
+    {
+        return $request->validate([
+            'name'=>['string','max:255'],
+            'username'=>['string','unique:users,username'],
+            'email'=>['required','email'],
+            'number'=>['required','max:11'],
+            'password'=>['required','min:8','max:255'],
+            'password_confirmation'=>['required','confirmed','min:8','max:255']
+        ]);
+    }
 }
 
 

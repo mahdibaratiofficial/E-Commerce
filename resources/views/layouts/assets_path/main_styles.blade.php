07bc48e538284d7c03c686bc7914a01a918bfb16
Mahdi <mahdibarati696@gmail.com>
2023-02-09 17:29:01 +0330

    I don't know what I did

--- resources/views/layouts/assets_path/main_styles.blade.php
+++ resources/views/layouts/assets_path/main_styles.blade.php
@@ -1,3 +1,4 @@
 <!-- Template CSS -->
 <link rel="stylesheet" href="{{ asset('assets/main/css/plugins/animate.min.css') }}" />
 <link rel="stylesheet" href="{{ asset('assets/main/css/main.css') }}" />
+<livewire:styles />

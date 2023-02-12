881ff4a77db813f911db2a5d9ed9003602b37728
Mahdi <mahdibarati696@gmail.com>
2023-02-12 23:05:31 +0330

    user Profile Controller for just open page

---
+++ app/Http/Controllers/Profile/ProfileController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Profile;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    public function getUser(User $user)
+    {
+        $userInstance = $user;
+        if(Auth::user()->username == $user->username)
+            return view('main.profile.profile', compact('userInstance'));
+    }
+}

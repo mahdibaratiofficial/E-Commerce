ffa31d8ef74870444a100503c2cf2b9f9298537d
Mahdi <mahdibarati696@gmail.com>
2023-02-13 18:02:49 +0330

    relation to product and complate fillable prop

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Vendor extends Model
 {
     use HasFactory;
+
+    protected $fillable=[
+        'vendor_name',
+        'rate',
+        'presentation',
+        'address',
+        'phone',
+        'email',
+        'socials',
+    ];
+
+    /**
+     * Relation to Product
+     *
+     * @return object 
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }

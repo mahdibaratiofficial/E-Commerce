31ae490a7d3a5f040b1368f9902ec2c7019c251c
Mahdi <mahdibarati696@gmail.com>
2023-02-13 20:05:04 +0330

    fake data factory

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -17,7 +17,12 @@ class CommentFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_id'=>1,
+            'body'=>$this->faker->paragraph(1),
+            'commentable_id'=>1,
+            'commentable_type'=>'App\Models\Product',
+            'like'=>rand(1,100),
+            'dislike'=>rand(2,40)
         ];
     }
 }

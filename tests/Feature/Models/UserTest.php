8c053c55782be8c688e3d636cc048ca2f00c3a24
Mahdi <mahdibarati696@gmail.com>
2023-02-14 20:39:22 +0330

    test method for check relation with vendor is currect

--- tests/Feature/Models/UserTest.php
+++ tests/Feature/Models/UserTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Models;
 
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Vendor;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Hash;
@@ -26,5 +27,16 @@ class UserTest extends TestCase
         $userCreated=User::create($user);
 
         $this->assertTrue(isset($userCreated->id));
+    }
+
+    public function testRelationWithVendors()
+    {
+        $user=User::factory()->has(Vendor::factory()->count(3))->create();
+
+        $this->assertTrue(is_array($user->Vendors->toArray()));
+
+        $this->assertTrue($user->vendors[0] instanceof Vendor);
+
+        $this->assertTrue(count($user->vendors->toArray())==3);
     }
 }

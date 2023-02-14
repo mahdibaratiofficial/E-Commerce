83d0e5ef12873ec147c39d74eb98b81bba3a1323
Mahdi <mahdibarati696@gmail.com>
2023-02-14 20:37:16 +0330

    change number faker value

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'number'=>substr($this->faker->phoneNumber(),11),
+            'number'=>rand(11111111111,99999999999),
             'username'=>$this->faker->userName(),
             'birth_day'=>now()->addYear(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password

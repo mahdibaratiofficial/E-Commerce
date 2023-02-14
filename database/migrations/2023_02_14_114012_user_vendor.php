8b8a8031d276f69cce32c046a4941da4c39c7e6f
Mahdi <mahdibarati696@gmail.com>
2023-02-14 20:40:39 +0330

    this table used for many to many relationship between vendor and user

---
+++ database/migrations/2023_02_14_114012_user_vendor.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_vendor',function(Blueprint $table){
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('user')->cascadeOnDelete();
+
+            $table->unsignedBigInteger('vendor_id');
+            $table->foreign('vendor_id')->references('id')->on('vendors')->cascadeOnDelete();
+
+            $table->primary(['user_id','vendor_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_vendor');
+    }
+};

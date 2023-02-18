198350de617e02e0c0a7fcc51214ac785916fa48
Mahdi <mahdibarati696@gmail.com>
2023-02-18 13:40:26 +0330

    relation with images

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,23 +2,26 @@
 
 namespace App\Models;
 
+use App\Models\Traits\Product as TraitsProduct;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable , TraitsProduct   ;
 
-    protected $fillable=[
+    protected $fillable = [
         'title',
         'descriotion',
         'rate',
         'vendor_id',
         'price',
-        'quantity'
+        'quantity',
+        'slug'
     ];
 
-    protected $table='products';
+    protected $table = 'products';
 
     /**
      * Relation to Vendor Table
@@ -37,11 +40,37 @@ class Product extends Model
      */
     public function comments()
     {
-        return $this->morphMany(Comment::class,'commentable');
+        return $this->morphMany(Comment::class, 'commentable');
     }
 
     public function Categories()
     {
-        return $this->belongsToMany(Category::class,'category_product');
+        return $this->belongsToMany(Category::class, 'category_product');
+    }
+
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source'=>'slug'
+            ]
+        ];
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+
+    public function images()
+    {
+        return $this->morphMany(Image::class,'imagable');
     }
 }

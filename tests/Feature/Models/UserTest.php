18fdd90612381ba5a2c1255b1da1fd6df881d5d6
Mahdi <mahdibarati696@gmail.com>
2023-02-18 13:45:09 +0330

    add method for relation with profile testing

--- tests/Feature/Models/UserTest.php
+++ tests/Feature/Models/UserTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Models;
 
 use App\Models\Post;
+use App\Models\ProfilePicture;
 use App\Models\User;
 use App\Models\Vendor;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -12,31 +13,32 @@ use Tests\TestCase;
 
 class UserTest extends TestCase
 {
-    use RefreshDatabase;
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testInsertUserData()
+    use RefreshDatabase, ModelHelperTesting;
+
+    protected function model()
     {
-        $user=User::factory()->make()->toArray();
-
-        $user['password'] = Hash::make('123456789');
-
-        $userCreated=User::create($user);
-
-        $this->assertTrue(isset($userCreated->id));
+        return new User();
     }
 
     public function testRelationWithVendors()
     {
-        $user=User::factory()->has(Vendor::factory()->count(3))->create();
+        $user = User::factory()->has(Vendor::factory()->count(3))->create();
 
         $this->assertTrue(is_array($user->Vendors->toArray()));
 
         $this->assertTrue($user->vendors[0] instanceof Vendor);
 
-        $this->assertTrue(count($user->vendors->toArray())==3);
+        $this->assertTrue(count($user->vendors->toArray()) == 3);
+    }
+
+    public function testRelationWithProfile()
+    {
+        $user=User::factory()->has(ProfilePicture::factory())->create();
+
+        $this->assertTrue(isset($user->profilePicture->id));
+
+        $this->assertTrue($user->profilePicture instanceof ProfilePicture);
+
+        $this->assertDatabaseHas('profile_pictures',$user->profilePicture->toArray());
     }
 }

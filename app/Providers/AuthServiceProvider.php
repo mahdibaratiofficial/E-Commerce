5bcb38364b386c7d924ca9f393881d878103eca1
Mahdi <mahdibarati696@gmail.com>
2023-03-16 16:43:17 +0330

    define Gates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\Permission;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +29,11 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        foreach(Permission::all() as $permission)
+        {
+            Gate::define($permission->name,function(User $user) use($permission){
+                return $this->isAllow($permission);
+            });
+        }
     }
 }
